afa6c3091860363ed10fce5495be72a39cdad66d
Denny <ciccia.denny@gmail.com>
2025-03-01 11:22:02 +0100

    refactor

--- classes/piatto.py
+++ classes/piatto.py
@@ -1,5 +1,6 @@
 from classes.costanti import *
 
+
 class Piatto:
     def __init__(self, nome, portata, base, ingredienti=None, ricetta="", consiglio_contorno=None):
         if portata in PORTATE:
@@ -16,54 +17,67 @@ class Piatto:
         self.__ingredienti = ingredienti
         self.__ricetta = ricetta
         self.__consiglio_contorno = consiglio_contorno
-        
+
+
     @property
     def nome(self):
         return self.__nome
-    
+
+
     @nome.setter
     def nome(self, value):
         self.__nome = value
+
 
     @property
     def portata(self):
         return self.__portata
 
+
     @portata.setter
     def portata(self, value):
         self.__portata = value
-        
+
+
     @property
     def base(self):
         return self.__base
 
+
     @base.setter
     def base(self, value):
         self.__base = value
-    
+
+
     @property
     def ingredienti(self):
         return self.__ingredienti
-        
+
+
     @ingredienti.setter
     def ingredienti(self, value):
         self.__ingredienti = value
-    
+
+
     @property
     def ricetta(self):
         return self.__ricetta
-        
+
+
     @ricetta.setter
     def ricetta(self, value):
         self.__ricetta = value
-        
+
+
     @property
     def consiglio_contorno(self):
         return self.__consiglio_contorno
 
+
     @consiglio_contorno.setter
     def consiglio_contorno(self, value):
         self.__consiglio_contorno = value
+
 
     def debug_print(self):
         print(f"nome: {self.nome}")
