db651a852d6cad7b9566cc1dca5f5a42efb4c868
Denny <ciccia.denny@gmail.com>
2025-03-01 11:47:36 +0100

    refactor

--- classes/lista_piatti.py
+++ classes/lista_piatti.py
@@ -4,70 +4,52 @@ import random
 import numpy
 from classes.piatto import Piatto
 
-class ListaPiatti:
+class ListaPiatti(list):
     # attributi di istanza
-    # __lista -> lista dei piatti
     # __ingredienti -> set di tutti gli ingredienti presenti nei piatti
 
-    def __init__(self, lista=None):
-        if lista is not None:
-            self.__lista = lista
-        else:
-            self.__lista = list()
 
+    def __init__(self):
+        super().__init__()
         self.__ingredienti = set()
 
-    def __iter__(self):
-        self.__index = 0
-        return self
-
-    def __next__(self):
-        if self.__index < len(self.__lista):
-            value = self.__lista[self.__index]
-            self.__index += 1
-            return value
-        else:
-            raise StopIteration
 
     @property
     def ingredienti(self):
         return list(self.__ingredienti)
 
-    def add(self, piatto):
-        self.__lista.append(piatto)
 
-    def read(self, nomefile):
-        dataframe = pandas.read_excel(nomefile)
+    def read_from_file(self, nomefile):
+        df = pandas.read_excel(nomefile)
+        df.columns = df.columns.str.replace(' ', '_')
 
         # esamino ogni riga del dataframe
-        for i in range(dataframe.index.stop):
-            row = dataframe.loc[i, :]  # dizionario
+        for row in df.itertuples()[1:]:
+            ingredienti = row.ingredienti.split(",")
 
-            # normalizzazione dei campi (perchè una cella vuota del foglio excel viene letta come NaN(float))
-            ingredienti = row["ingredienti"].split(",")
-
-            if not isinstance(row["ricetta"], numpy.float64) and not isinstance(row["ricetta"], float):
-                ricetta = row["ricetta"]
+            if not isinstance(row.ricetta, numpy.float64) and not isinstance(row.ricetta, float):
+                ricetta = row.ricetta
             else:
                 ricetta = None
 
-            if not isinstance(row["consiglio contorno"], numpy.float64) and not isinstance(row["consiglio contorno"], float):
-                consiglio_contorno = row["consiglio contorno"].split(",")
+            if not isinstance(row.consiglio_contorno, numpy.float64) and not isinstance(row.consiglio_contorno, float):
+                consiglio_contorno = row.consiglio_contorno.split(",")
             else:
                 consiglio_contorno = None
 
             # costruzione piatto e inserimento nella lista dei piatti
-            piatto = Piatto(row["nome"], row["portata"], row["base"], ingredienti, ricetta, consiglio_contorno)
-            self.__lista.append(piatto)
+            piatto = Piatto(row.nome, row.portata, row.base, ingredienti, ricetta, consiglio_contorno)
+            self.append(piatto)
 
             # aggiornamento lista ingredienti
             for ingr in ingredienti:
                 self.__ingredienti.add(ingr)
 
+
     def piatto_a_caso(self, portata, base=None, ingredienti=None):
         lista_temp = list()
 
-        for p in self.__lista:
+        for p in self:
             if self.__piatto_idoneo(p,portata,base,ingredienti):
                 lista_temp.append(p)
 
@@ -78,14 +60,16 @@ class ListaPiatti:
 
         return piatto
 
+
     def tutti_i_piatti(self, portata, base=None, ingredienti=None):
         piatti_conformi = ListaPiatti()
 
-        for piatto in self.__lista:
+        for piatto in self:
             if self.__piatto_idoneo(piatto,portata,base,ingredienti):
                 piatti_conformi.add(piatto)
 
         return piatti_conformi
+
 
     @staticmethod
     def elemento_comune(list1, list2):
@@ -93,6 +77,7 @@ class ListaPiatti:
             if operator.countOf(list1,elem) > 0:
                 return True
         return False
+
 
     def __piatto_idoneo(self, piatto, portata, base=None, ingredienti=None):
         idoneo = False
@@ -113,6 +98,3 @@ class ListaPiatti:
                 idoneo = False
 
         return idoneo
-
-    def get_ingr_number(self):
-        return len(self.__ingredienti)
